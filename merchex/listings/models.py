d8c463bfc614188a7915b8780f1a2745ab977605
Gauthier <g.p220498@gmail.com>
2023-05-02 15:44:32 +0200

    Modification de admin.py et models.py pour CRUD dans admin django

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -16,12 +16,15 @@ class Band(models.Model):
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
-        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(
         null=True, blank=True
     )  # Autorise les valeurs nuls si un groupe n'a pas de sites
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Listing(models.Model):
@@ -32,7 +35,10 @@ class Listing(models.Model):
         MISCELLANEOUS = "MIS"
 
     title = models.fields.CharField(max_length=100)
-    description = models.fields.CharField(max_length=280)
+    description = models.fields.CharField(max_length=1000)
     sold = models.fields.BooleanField(default=False)
     year = models.fields.IntegerField()
     type = models.fields.CharField(choices=Type.choices, max_length=5)
+
+    def __str__(self):
+        return f"{self.title}"
