ab9ef66430d7e76b6515d3196c878de2bc25e1e5
Gauthier <g.p220498@gmail.com>
2023-05-02 12:27:25 +0200

    Ajout de nouveaux champs aux modéles Band et Listing

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,9 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
+    class Genre(
+        models.TextChoices
+    ):  # Cette héritage permet de créer une liste de choix
+        HIP_HOP = "HH"
+        SYNTH_POP = "SP"
+        ALTERNATIVE_ROCK = "AR"
+        ELECTRO_HARDSTYLE = "HS"
+        LATINO = "LT"
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(
+        null=True, blank=True
+    )  # Autorise les valeurs nuls si un groupe n'a pas de sites
 
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = "REC"
+        CLOTHING = "CLT"
+        POSTERS = "PST"
+        MISCELLANEOUS = "MIS"
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=280)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField()
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
